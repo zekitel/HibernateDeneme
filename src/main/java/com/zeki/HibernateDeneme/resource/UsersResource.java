d6b106841af1e33cc0600ec2f19b0333d7db63e8
zeki.tel <zeki.tel@intra.local>
2018-07-10 10:05:53 +0300

    delete,update,insert eklendi test edilecek

--- src/main/java/com/zeki/HibernateDeneme/resource/UsersResource.java
+++ src/main/java/com/zeki/HibernateDeneme/resource/UsersResource.java
@@ -2,16 +2,17 @@ package com.zeki.HibernateDeneme.resource;
 
 import com.zeki.HibernateDeneme.repository.UserRepository;
 import com.zeki.HibernateDeneme.model.Users;
+import org.apache.catalina.User;
 import org.json.JSONArray;
+import org.omg.CORBA.Request;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.jws.soap.SOAPBinding;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/rest/users")
@@ -26,6 +27,29 @@ public class UsersResource  {
         
         return userRepository.findAll();
     }
+
+    @GetMapping(value ="/getById")
+    public Users getUserById(@RequestParam int id ){
+        return userRepository.getOne();
+
+    }
+
+    @PostMapping("/insert")
+    public void insertUser(Users users){
+        this.userRepository.save(users);
+    }
+
+    @DeleteMapping("/delete")
+    public void deleteUser(@RequestParam int id){
+        this.userRepository.deleteById(id);
+    }
+    @PostMapping("update")
+    public void updateUser(Users users){
+        this.userRepository.deleteById(users.getId());
+        this.userRepository.save(users);
+
+    }
+
 
 
 }
